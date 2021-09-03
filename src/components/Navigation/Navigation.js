27c780038add979a13d2bce8ed0f3d4f882be714
lefagana <malahmusalg@gmail.com>
2021-09-03 21:27:13 +0100

    hello

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Navigation = ({onRouteChange, isSignedIn }) =>{
+        if (isSignedIn) {
+            return(
+                <nav style={{display:'flex', justifyContent:'flex-end'}}>
+                    <p
+                    onClick={ () => onRouteChange('signout')} 
+                    className='f3 link dim black underline pa3 pointer'>Sign Out</p>    
+                </nav>
+            )
+        }else{
+            return(
+                    <nav style={{display:'flex', justifyContent:'flex-end'}}>
+                        <p
+                            onClick={ () => onRouteChange('SignIn')} 
+                            className='f3 link dim black underline pa3 pointer'>SignIn
+                        </p>
+                        <p
+                            onClick={ () => onRouteChange('Register')} 
+                            className='f3 link dim black underline pa3 pointer'>Register
+                        </p>    
+                    </nav>
+            );
+        }    
+    
+}
+
+export default Navigation;
